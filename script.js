31544dcd8ad2ef60951521bb2e5a353e0c3c9aa4
WilliamMatiasDeOLiveira <williammatiasjau@hotmail.com>
2025-07-10 21:00:24 -0300

    adição do slide transition da pagina home

---
+++ script.js
@@ -0,0 +1,23 @@
+const slides = document.querySelectorAll('.container')
+
+let current = 0
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.classList.add('active')
+        }else{
+            slide.classList.remove('active')
+        }
+    })
+}
+
+function nextSlide(){
+    current = (current + 1) % slides.length
+    showSlide(current)
+}
+
+showSlide(current)
+
+setInterval(nextSlide, 10000)
+
